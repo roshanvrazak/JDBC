1e8d056a42007a1d24d2326c7479493f81bcad33
RVR <RVR@172.16.1.223>
2018-08-10 20:37:09 +0530

    First Commit

---
+++ src/com/caps/jsp/MyFirstJDBCProgram.java
@@ -0,0 +1,89 @@
+package com.caps.jsp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Driver; //required for jdbc to connect to mySql database
+
+public class MyFirstJDBCProgram {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			/*
+			 * 1. Load the Driver
+			 */
+			java.sql.Driver driverRef = new Driver();
+			DriverManager.registerDriver(driverRef);
+			System.out.println("Driver Loaded...");
+			/*
+			 * 2. Get the DB Connection via Driver
+			 */
+			String dbUrl = "jdbc:mysql://localhost:3306/capsV3_db" + "?user=root&password=root";
+			con = DriverManager.getConnection(dbUrl);
+			System.out.println("Connected...");
+			/*
+			 * 3. Issue the SQL query via connection
+			 */
+			String sql = "select * from students_info";
+
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+
+			/*
+			 * 4. Process the results
+			 */
+
+			while (rs.next()) {
+				int regno = rs.getInt("sid");
+				String firstname = rs.getString("firstname");
+				String lastname = rs.getString("lastname");
+				String isAdmin = rs.getString("isadmin");
+				String passwd = rs.getString("password");
+
+				System.out.println("Reg no: " + regno);
+				System.out.println("First name: " + firstname);
+				System.out.println("Last Name: " + lastname);
+				System.out.println("Admin: " + isAdmin);
+				System.out.println("Password: " + passwd);
+				System.out.println("-------------------");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			/*
+			 * 5. Close all the JDBC Objects
+			 */
+
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}// end of main
+
+}// End of Class
